76c929447e9cc73472c5c1dd829e1cd97531ab3d
José Daniel Grayson <josedanielgrayson@proton.me>
2025-06-26 12:45:35 -0500

    build: add env variables to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,14 @@
   pkgs ? import <nixpkgs> { },
 }:
 pkgs.mkShell {
+  env = {
+    PGHOST = "localhost";
+    PGPORT = "5555";
+    PGDATABASE = "postgres";
+    PGUSER = "postgres";
+    PGPASSWORD = "123";
+  };
+
   nativeBuildInputs = with pkgs; [
     postgresql
     (python3.withPackages (
